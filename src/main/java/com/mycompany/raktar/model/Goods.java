a87cfe3d11e817be6a86f7ca65cea14cbe4e39cc
Kovács Gergő <Kovcs Gerg@Xetix-PC>
2021-03-04 19:21:43 +0100

    Build Goods class

--- src/main/java/com/mycompany/raktar/model/Goods.java
+++ src/main/java/com/mycompany/raktar/model/Goods.java
@@ -11,18 +11,42 @@ package com.mycompany.raktar.model;
  */
 public class Goods {
     private int articleNumber; //Cikkszám
+    
     private String name; //Név
+    private String vendor; //Gyártó
     private String description; //Leírás
+    
     private int stock;  //Készlet
     private String unitOfMeasure; //Mértékegység
+    
     private int price; //Ár
     private Currency currency; //Pénznem
     
-    private enum Currency{
+    private enum Currency {
         HUF, USD, GBP, EUR, JPY, RUB, CNY
     }
     
-    private enum UnitOfMeasure{
+    private enum UnitOfMeasure {
         db, cm, m, g, kg, csomag
+    }
+
+    public Goods(
+        int articleNumber, 
+        String name, 
+        String vendor, 
+        String description, 
+        int stock, 
+        String unitOfMeasure, 
+        int price, 
+        Currency currency
+    ) {
+        this.articleNumber = articleNumber;
+        this.name = name;
+        this.vendor = vendor;
+        this.description = description;
+        this.stock = stock;
+        this.unitOfMeasure = unitOfMeasure;
+        this.price = price;
+        this.currency = currency;
     }
 }
