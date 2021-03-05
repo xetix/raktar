1c90153bc2251a834d5e2c0045fa1187056d0d63
Kovács Gergő <Kovcs Gerg@Xetix-PC>
2021-03-05 10:01:58 +0100

    add Warehouse class

---
+++ src/main/java/com/mycompany/raktar/model/Warehouse.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.raktar.model;
+
+import com.mycompany.raktar.model.Category;
+import java.util.List;
+
+/**
+ *
+ * @author Kovács Gergő
+ */
+public class Warehouse {
+    private List<Category> categories;
+
+    public Warehouse() {
+    }
+
+    public Warehouse(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+    
+    public void addCategory(Category category) {
+        this.categories.add(category);
+    }
+    
+    public void delCategory(Category category){
+        this.categories.remove(category);
+    }
+
+    @Override
+    public String toString() {
+        return "Warehouse{" + "categories=" + categories + '}';
+    }  
+    
+}
