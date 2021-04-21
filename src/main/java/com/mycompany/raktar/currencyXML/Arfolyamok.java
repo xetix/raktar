b1b40b56c1586804bd3151bf5deaeedd7e9b4086
bricsi98 <bricsi1998@gmail.com>
2021-04-21 23:55:07 +0200

    napiarfolyam.hu API használata az árfolyamok frissentartására implementálva; XML letöltése, és feldolgozása

---
+++ src/main/java/com/mycompany/raktar/currencyXML/Arfolyamok.java
@@ -0,0 +1,20 @@
+package com.mycompany.raktar.currencyXML;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Arfolyamok {
+    private Deviza deviza;
+
+    public Arfolyamok() {
+    }
+    public Arfolyamok(Deviza deviza) {
+        super();
+        this.deviza=deviza;
+    }
+}
